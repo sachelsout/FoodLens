d6718ddabd3a0d6cb376fd942d4aa1a82381bd30
Rohan Dawkhar <rohan.dawkhar@gmail.com>
2025-09-17 03:27:46 -0400

    removed ping and test files

--- v1/api/ping.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.route("/", defaults={"path": ""}, methods=["GET"]) 
-@app.route("/<path:path>", methods=["GET"]) 
-def ping(path: str = ""):
-    return jsonify({"ok": True})
